6aca87857fbbaaa0dc18e18bc5d4d94bc19f6f8c
Leo Kyrkos <Leo.Kyrkos@yourfavouritestory.com>
2017-11-27 12:17:29 +0000

    Fixed hello world test. Updated test config. Added blank readme to specs to keep the dir when cloning

--- src/js/components/HelloWorld/HelloWorld.spec.js
+++ src/js/components/HelloWorld/HelloWorld.spec.js
@@ -6,7 +6,7 @@ describe("HelloWorld.vue", () => {
 		const Constructor = Vue.extend(HelloWorld);
 		const vm = new Constructor().$mount();
 		expect(vm.$el.querySelector("h1").textContent).to.equal(
-			"Hello world - This is a title"
+			"Hello world"
 		);
 	});
 });
