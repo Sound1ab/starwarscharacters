8e97a1274b71bb4a606b851adf0b2c52b7574952
Leo Kyrkos <Leo.Kyrkos@yourfavouritestory.com>
2017-11-30 17:37:41 +0000

    Switched out karma/mocha/chai for jest

--- src/js/components/HelloWorld/HelloWorld.spec.js
+++ src/js/components/HelloWorld/HelloWorld.spec.js
@@ -5,7 +5,7 @@ describe("HelloWorld.vue", () => {
 	it("should render correct contents", () => {
 		const Constructor = Vue.extend(HelloWorld);
 		const vm = new Constructor().$mount();
-		expect(vm.$el.querySelector("h1").textContent).to.equal(
+		expect(vm.$el.querySelector("h1").textContent).toEqual(
 			"Hello world"
 		);
 	});
