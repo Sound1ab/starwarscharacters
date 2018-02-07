3a7cc5773bebe4849d1dc2b13930b82c9a17f2e8
Leo Kyrkos <leo.kyrkos@yourfavouritestory.com>
2018-02-07 14:01:58 +0000

    Updated to incorporate recent changes to vue-cli.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
         browser: true,
         es6: true
     },
-    extends: "eslint:recommended",
+    extends: [
+        // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+        // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+        'plugin:vue/essential', 
+        // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    ],
     parserOptions: {
         ecmaFeatures: {
             jsx: true
