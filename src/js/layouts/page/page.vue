dd58ff1b6ec500747442dd34be9e7b03ce883acb
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-02 10:59:19 +0100

    Package.json update

---
+++ src/js/layouts/page/page.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="page">
+	    hey
+	</div>
+</template>
+
+<script>
+	import VueTypes from 'vue-types';
+	export default {
+		name: 'page'
+	};
+</script>
+
+<style lang="scss" type="text/scss">
+	.page {
+		position: relative;
+	}
+</style>
