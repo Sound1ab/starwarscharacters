854c1cbcc6298b0a62b8fe5e9b5ca72308a5e1ff
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-02 11:00:18 +0100

    Add function to load data when app starts

--- src/js/layouts/page/page.vue
+++ src/js/layouts/page/page.vue
@@ -6,8 +6,31 @@
 
 <script>
 	import VueTypes from 'vue-types';
+	import {FETCH_TRANSITION} from '@/js/vuex/modules/fetch';
 	export default {
-		name: 'page'
+		name: 'page',
+		methods: {
+			dispatchFetch (page) {
+				if (!page) {
+					return;
+				}
+				console.log(page);
+				this.$store.dispatch(FETCH_TRANSITION, {
+					type: 'FETCH_DATA',
+					params: {
+						type: 'PAGE',
+						page
+					}
+				});
+			},
+			onLoad () {
+				const page = this.$route.params.id;
+				this.dispatchFetch(page);
+			}
+		},
+		created () {
+			this.onLoad();
+		}
 	};
 </script>
 
