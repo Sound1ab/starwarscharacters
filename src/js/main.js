df84183709a15b8232f54e9411583dc1b093f627
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-02 14:06:54 +0100

    Add vuesvg

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,12 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import * as svgicon from 'vue-svgicon';
 import globalStyles from '@styles/global.scss';
 
 Vue.config.productionTip = false;
+
+Vue.use(svgicon);
 
 /* eslint-disable no-new */
 new Vue({
