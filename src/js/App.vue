fdd30f479d6a1e2838a628507adc1b3410afa7d1
Phillip Parker <phillip.parker@yourfavouritestory.com>
2018-04-02 12:44:58 +0100

    Add pagination component

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,13 +1,30 @@
 <template>
 	<div id="app">
+		<search></search>
 		<router-view/>
+		<pagination></pagination>
 	</div>
 </template>
 
 <script>
 	import store from './vuex/index';
+	import Search from '@/js/components/Search/search';
+	import Pagination from '@/js/components/Pagination/pagination';
 	export default {
 		name: 'app',
-		store
+		store,
+		components: {
+			Pagination,
+			Search
+		}
 	};
 </script>
+
+<style lang="scss" type="text/scss">
+	#app {
+		height: 100%;
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-template-rows: 3fr;
+	}
+</style>
---
+++ src/js/components/Pagination/pagination.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="pagination">
+	    <span>Previous</span>
+		{{$route.params.id}}
+	    <span>Next</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'pagination'
+	};
+</script>
+
+<style lang="scss" type="text/scss">
+	.pagination {
+		padding: em(40)
+	}
+</style>
